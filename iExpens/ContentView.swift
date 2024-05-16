3f3cc6a2f56c1d0b5d44dd8492535150f99af4c4
Álvaro Gascón <alvarogascon@hotmail.com>
2024-05-16 09:01:31 +0200

    Initial Commit

---
+++ iExpens/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iExpens
+//
+//  Created by Álvaro Gascón on 16/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iExpens/iExpensApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpensApp.swift
+//  iExpens
+//
+//  Created by Álvaro Gascón on 16/5/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
